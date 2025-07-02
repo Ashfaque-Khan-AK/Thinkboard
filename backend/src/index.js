8f648b94f3d72b48d7b27f7daadee689dd9545b4
Ashfaque-Ahmed47 <ishfaque7878@gmail.com>
2025-07-02 22:39:57 +0500

    updated the production code inside index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,16 +26,13 @@ app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
 
-if (process.env.NODE_ENV ==="production") {
-  
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.get("*", (req, res) =>{
-  res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-});
-
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
-
 connectDB().then(()=>{
     
     app.listen(PORT, () =>  console.log(`Server started on port: ${PORT}`));
